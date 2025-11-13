31ce6c12b34eb9a5485b4d326b363a58c99acfe5
Etienne <etienne.moussa@etu.umontpellier.fr>
2025-11-13 14:13:42 +0100

    fix Dictionnary and Index: Replaceing String by Term in all files

--- src/main/java/qengine/storage/RDFHexaStore.java
+++ src/main/java/qengine/storage/RDFHexaStore.java
@@ -1,9 +1,12 @@
 package qengine.storage;
 
 import fr.boreal.model.logicalElements.api.*;
+import fr.boreal.model.logicalElements.factory.api.TermFactory;
+import fr.boreal.model.logicalElements.factory.impl.SameObjectTermFactory;
 import org.apache.commons.lang3.NotImplementedException;
 import qengine.model.RDFTriple;
 import qengine.model.StarQuery;
+import qengine.dictionnary.RDFDictionnary;
 
 import java.util.*;
 
@@ -22,15 +25,43 @@ public class RDFHexaStore implements RDFStorage {
     Map<Integer, Map<Integer, List<Integer>>> OPS;
     Map<Integer, Map<Integer, List<Integer>>> OSP;
 
+    private final RDFDictionnary dict = RDFDictionnary.getInstance();
+    private TermFactory termFactory;
+
 
     @Override
     public boolean add(RDFTriple triple) {
-        throw new NotImplementedException();
+        termFactory = SameObjectTermFactory.instance();
+
+        int s = dict.encode(termFactory.createOrGetLiteral(triple.getTripleSubject().toString()));
+        int o = dict.encode(termFactory.createOrGetLiteral(triple.getTripleObject().toString()));
+        int p = dict.encode(termFactory.createOrGetLiteral(triple.getTriplePredicate().toString()));
+
+        // Ajout dans les 6 index
+        addToIndex(SPO, s, p, o);
+        addToIndex(SOP, s, o, p);
+        addToIndex(PSO, p, s, o);
+        addToIndex(POS, p, o, s);
+        addToIndex(OSP, o, s, p);
+        addToIndex(OPS, o, p, s);
+
+        return true;
+
+    }
+
+    // Fonction d'ajout annexe
+    private void addToIndex(Map<Integer, Map<Integer, List<Integer>>> index, int first, int second, int third) {
+        index.computeIfAbsent(first, k -> new HashMap<>())
+                .computeIfAbsent(second, k -> new ArrayList<>())
+                .add(third);
     }
 
     @Override
     public long size() {
-        throw new NotImplementedException();
+        return SPO.values().stream()
+                .flatMap(m -> m.values().stream())
+                .mapToLong(List::size)
+                .sum();
     }
 
     @Override
@@ -50,6 +81,21 @@ public class RDFHexaStore implements RDFStorage {
 
     @Override
     public Collection<RDFTriple> getAtoms() {
-        throw new NotImplementedException();
+        List<RDFTriple> triples = new ArrayList<>();
+
+        for (var sEntry : SPO.entrySet()) {
+            int s = sEntry.getKey();
+            for (var pEntry : sEntry.getValue().entrySet()) {
+                int p = pEntry.getKey();
+                for (int o : pEntry.getValue()) {
+                    Term subj = dict.decode(s);
+                    Term pred = dict.decode(p);
+                    Term obj = dict.decode(o);
+
+                    triples.add(new RDFTriple(subj, pred, obj));
+                }
+            }
+        }
+        return triples;
     }
 }
--- src/test/java/qengine/dictionary/RDFDictionnaryTest.java
+++ src/test/java/qengine/dictionary/RDFDictionnaryTest.java
@@ -1,5 +1,8 @@
 package qengine.dictionary;
 
+import fr.boreal.model.logicalElements.api.Term;
+import fr.boreal.model.logicalElements.factory.api.TermFactory;
+import fr.boreal.model.logicalElements.factory.impl.SameObjectTermFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import qengine.dictionnary.RDFDictionnary;
@@ -9,42 +12,70 @@ import static org.junit.jupiter.api.Assertions.*;
 public class RDFDictionnaryTest {
 
     private RDFDictionnary dict;
+    private TermFactory termFactory;
 
     @BeforeEach
     public void setUp() {
         // Comme getInstance() ne garantit pas vraiment un vrai singleton (erreur dans la classe),
         // on crée manuellement une nouvelle instance pour isoler les tests.
         dict = RDFDictionnary.getInstance();
+        termFactory = SameObjectTermFactory.instance();
     }
 
     @Test
     public void testEncodeReturnsUniqueIds() {
-        int id1 = dict.encode("http://example.org/Alice");
-        int id2 = dict.encode("http://example.org/Bob");
+        Term term1 = termFactory.createOrGetLiteral("<http://example.org/Alice>");
+        Term term2 = termFactory.createOrGetLiteral("<http://example.org/Bob>");
+        
+        int id1 = dict.encode(term1);
+        int id2 = dict.encode(term2);
 
         assertNotEquals(id1, id2, "Chaque ressource doit avoir un ID unique");
     }
 
     @Test
     public void testEncodeSameResourceReturnsSameId() {
-        int id1 = dict.encode("http://example.org/Alice");
-        int id2 = dict.encode("http://example.org/Alice");
+        Term term = termFactory.createOrGetLiteral("<http://example.org/Alice>");
+        
+        int id1 = dict.encode(term);
+        int id2 = dict.encode(term);
 
         assertEquals(id1, id2, "La même ressource doit toujours avoir le même ID");
     }
 
     @Test
     public void testDecodeReturnsOriginalResource() {
-        String resource = "http://example.org/Charlie";
+        Term resource = termFactory.createOrGetLiteral("<http://example.org/Charlie>");
         int id = dict.encode(resource);
 
-        String decoded = dict.decode(id);
+        Term decoded = dict.decode(id);
         assertEquals(resource, decoded, "Le décodage doit renvoyer la ressource originale");
     }
 
     @Test
     public void testDecodeUnknownIdReturnsNull() {
         assertNull(dict.decode(9999), "Décoder un ID inconnu doit renvoyer null");
+    }
+
+    @Test
+    public void testEncodeVariable() {
+        Term variable = termFactory.createOrGetVariable("?x");
+        int id = dict.encode(variable);
+        
+        Term decoded = dict.decode(id);
+        assertEquals(variable, decoded, "Le décodage d'une variable doit renvoyer la variable originale");
+    }
+
+    @Test
+    public void testMultipleEncodingsPreserveEquality() {
+        Term term1 = termFactory.createOrGetLiteral("<http://example.org/Alice>");
+        Term term2 = termFactory.createOrGetLiteral("<http://example.org/Alice>");
+        
+        // Avec SameObjectTermFactory, les deux références devraient être identiques
+        int id1 = dict.encode(term1);
+        int id2 = dict.encode(term2);
+        
+        assertEquals(id1, id2, "Des termes identiques doivent avoir le même ID");
     }
 
 }
