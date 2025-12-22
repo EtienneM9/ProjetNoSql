d7c3f268cb450908f82c3c827ca242784f80d640
Etienne <etienne.moussa@etu.umontpellier.fr>
2025-12-22 15:29:38 +0100

    Fixed the fix

--- src/main/java/qengine/storage/RDFHexaStore.java
+++ src/main/java/qengine/storage/RDFHexaStore.java
@@ -39,12 +39,8 @@ public class RDFHexaStore implements RDFStorage {
         int o = dict.encode(termFactory.createOrGetLiteral(triple.getTripleObject().toString()));
         int p = dict.encode(termFactory.createOrGetLiteral(triple.getTriplePredicate().toString()));
 
-        if (!SPO.containsKey(s)){
+        if (SPO.containsKey(s) && SPO.get(s).containsKey(p) && SPO.get(s).get(p).contains(o)){
             return false; // Le triplet existe déjà, on ne fait rien
-        } else if (!SPO.get(s).containsKey(p)) {
-            return false;
-        } else if (!SPO.get(s).get(p).contains(o)) {
-            return false;
         }
 
         // Ajout dans les 6 index
